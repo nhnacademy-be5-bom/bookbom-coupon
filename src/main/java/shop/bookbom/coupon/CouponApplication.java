9fb47d90eee4eebde757e6c407f136f9a7d6831d
youaresogay22 <youaresogayee@gmail.com>
2024-03-28 13:30:19 +0900

    [Feature] eureka 적용, 개발환경 분리

--- src/main/java/shop/bookbom/coupon/CouponApplication.java
+++ src/main/java/shop/bookbom/coupon/CouponApplication.java
@@ -2,8 +2,12 @@ package shop.bookbom.coupon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class CouponApplication {
 
 	public static void main(String[] args) {
